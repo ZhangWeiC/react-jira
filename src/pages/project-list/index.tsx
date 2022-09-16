aa8422b1973532dd2db83a2d069031f3f7da26c7
zhangweichen <zhangweichen.814@bytedance.com>
2022-09-16 18:44:57 +0800

    feat: init

---
+++ src/pages/project-list/index.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { SearchPanel } from "./search-panel"
+import { List } from "./list"
+import {useState, useEffect} from "react"
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import * as qs from 'qs';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const debouncedParam = useDebounce(param, 1000);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+      if(response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [debouncedParam])
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if(response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  })
+
+  return <div>
+    <SearchPanel param={param} setParam={setParam} users={users}/>
+    <List list={list} users={users}/>
+  </div>
+}
+
